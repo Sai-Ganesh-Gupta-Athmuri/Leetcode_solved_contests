657beb34a93f8d833f460ba02529a890ba17e46e
Athmuri V V A Sai Ganesh Gupta <107981649+Sai-Ganesh-Gupta-Athmuri@users.noreply.github.com>
2023-12-31 09:09:49 +0530

    Update 100185. Find Longest Special Substring That Occurs Thrice I.java

--- 100185. Find Longest Special Substring That Occurs Thrice I.java
+++ 100185. Find Longest Special Substring That Occurs Thrice I.java
@@ -1,31 +1,45 @@
+// You are given a string s that consists of lowercase English letters.
+
+// A string is called special if it is made up of only a single character. For example, the string "abc" is not special, whereas the strings "ddd", "zz", and "f" are special.
+
+// Return the length of the longest special substring of s which occurs at least thrice, or -1 if no special substring occurs at least thrice.
+
+// A substring is a contiguous non-empty sequence of characters within a string.
+
+// Constraints:
+
+// 3 <= s.length <= 50
+// s consists of only lowercase English letters.
+
 class Solution {
     public int maximumLength(String s) {
-        int res = 0;
-        ArrayList<ArrayList<Integer>> arr = new ArrayList<>(26);
-        for (int i = 0; i < 26; i++) {
-            arr.add(new ArrayList<>());
-        }
-        for (int i = 0; i < s.length(); i++) {
-            int j = i;
-            while (j < s.length() && s.charAt(j) == s.charAt(i)) {
-                j++;
-            }
-            arr.get(s.charAt(i) - 'a').add(j - i);
-            i = j - 1;
-        }
-        for (ArrayList<Integer> ele : arr) {
-            if (ele.isEmpty()) continue;
-            ele.sort(Integer::compareTo);
-            int size = ele.size();
-            res = Math.max(res, ele.get(size - 1) - 2);
-            if (size > 1) {
-                res = Math.max(res, Math.min(ele.get(size - 1) - 1, ele.get(size - 2)));  
-            }
-            if (size > 2) {
-                res = Math.max(res, ele.get(size - 3));  
+        int n = s.length();
+        HashMap<String,Integer> map = new HashMap<>();
+        for(int i = 0;i<n;i++){
+            for(int j = i;j<n;j++){
+                String str = s.substring(i,j+1);
+                boolean check = true;
+                for(int k = 1;k<str.length();k++){
+                    if(str.charAt(k) != str.charAt(k-1)){
+                        check = false;
+                        break;
+                    } 
+                }
+                if(check && map.containsKey(str)){
+                    int f = map.get(str);
+                    map.put(str,f+1);
+                }else if(check && !map.containsKey(str)){
+                    map.put(str,1);
+                }
             }
         }
-        if (res == 0) res = -1;
-        return res;
+        int max = -1;
+        int c = 0;
+        for(String key : map.keySet()){
+            if(map.get(key) >= 3 && key.length()>max){
+                max = key.length();
+            }
+        }
+        return max;
     }
 }
